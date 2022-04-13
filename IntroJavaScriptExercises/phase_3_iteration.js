c66c65afbefe2f5239be9d910a4c3791d2ab9ef5
AA Student <appacademy@Mini-34.local>
2022-04-12 17:30:27 -0700

    finish phase III

--- IntroJavaScriptExercises/phase_3_iteration.js
+++ IntroJavaScriptExercises/phase_3_iteration.js
@@ -24,3 +24,21 @@ const array = [1,9,4,5,7,2,8,3,6]
 
 console.log(array.bubbleSort(sortRule));
 console.log("(-----)")
+
+String.prototype.subS = function() {
+
+    let arr = [];
+    let splitThis = this.split("");
+    splitThis.forEach((el,index) => {
+
+        splitThis.forEach((el2,index2) => {
+
+            if (!arr.includes(this.slice(index,index2))) {
+                arr.push(this.slice(index,index2))
+            }
+        })
+    })
+    return arr;
+}
+const hello = "hello"
+console.log(hello.subS());
